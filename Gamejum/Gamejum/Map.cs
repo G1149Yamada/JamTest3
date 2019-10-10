2af8896ef73f8d07d0319ee032eac3f775f05358
yamadayuji <yu46883@gmail.com>
2019-10-10 16:19:47 +0900

    使える状態になったはず

---
+++ Gamejum/Gamejum/Map.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gamejum
+{
+    class Map
+    {
+        MapChip mapChip = new MapChip();
+        Block block = new Block();
+        private int[,] map;
+
+        private Vector2 position;
+        private float whith, hight;
+
+        public Map()
+        {
+            map = mapChip.map;
+        }
+
+        public void Initialize()
+        {
+            GetPosition();
+        }
+
+        public void GetPosition()
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[x, y] == 1)
+                    {
+                        position = new Vector2(x * whith, y * hight);
+                        block.SetPosition(position);
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
