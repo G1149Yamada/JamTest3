963178186470b3226c6659e19a974f4dbefef7b4
山口アンドレ <k0189G1575.andreluys@gmail.com>
2019-10-17 20:00:39 +0900

    コミットとプッシュのテスト

--- Gamejum/Gamejum/Game1.cs
+++ Gamejum/Gamejum/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Gamejum
+namespace Gamejum //コミットとプッシュのテスト
 {
     /// <summary>
     /// This is the main type for your game.
--- Gamejum/Gamejum/MapChip1.cs
+++ Gamejum/Gamejum/MapChip1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Gamejum
+namespace Gamejum //　コミットとプッシュのテスト
 {
     class MapChip1
     {
