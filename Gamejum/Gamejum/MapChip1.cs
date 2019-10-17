c7da6a560f321ba9ec81cbf267e8241683c8f029
山口アンドレ <k0189G1575.andreluys@gmail.com>
2019-10-17 20:04:00 +0900

    テスト

--- Gamejum/Gamejum/MapChip1.cs
+++ Gamejum/Gamejum/MapChip1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Gamejum //　コミットとプッシュのテスト
+namespace Gamejum 
 {
     class MapChip1
     {
